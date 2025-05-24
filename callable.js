5aa59b8cc4ccb9cddecf40bc6b1bca2297adb565
keagonbeatty <beatt1ks@gmail.com>
2025-05-24 16:58:30 -0400

    made some changes

--- callable.js
+++ callable.js
@@ -1,18 +1,110 @@
-function sendDailyEmail() {
-  const sheet =
-    SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Config_Sheet");
-  const data = sheet.getRange("A1:B5").getValues();
+function getValue(cell) {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Email");
 
-  const config_data = sheet.getRange("A1:B5");
+  return sheet.getRange(cell).getValue();
+}
 
-  let body = "Here is your daily update:\n\n";
-  data.forEach((row) => {
-    body += row.join(" | ") + "\n";
-  });
+function sendEmailWithChart() {
+  const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = spreadsheet.getSheetByName("Email");
+  const charts = sheet.getCharts();
 
-  MailApp.sendEmail({
-    to: "your_email@example.com",
-    subject: "Daily Report from Google Sheet",
-    body: body,
+  const goals = {
+    section1: {
+      goal1: {
+        value: getValue("J9"),
+        class: sheet,
+      },
+      goal2: {
+        value: sheet.getRange("J10").getValue(),
+        class: sheet.getRange("J10").getValue() > 3.0 ? "green" : "red",
+      },
+      goal3: {
+        value: sheet.getRange("J11").getValue(),
+        class: sheet.getRange("J11").getValue() < 86 ? "green" : "red",
+      },
+      goal4: {
+        value: sheet.getRange("J12").getValue(),
+        class: sheet.getRange("J12").getValue() < 86 ? "green" : "red",
+      },
+    },
+    section2: {
+      goal1: {
+        value: sheet.getRange("J14").getValue(),
+        class: sheet.getRange("J14").getValue() < 60 ? "green" : "red",
+      },
+      goal2: {
+        value: sheet.getRange("J15").getValue(),
+        class: sheet.getRange("J15").getValue() > 3.0 ? "green" : "red",
+      },
+      goal3: {
+        value: sheet.getRange("J16").getValue(),
+      },
+      goal4: {
+        value: sheet.getRange("J17").getValue(),
+      },
+    },
+    section3: {
+      goal1: {
+        value: sheet.getRange("J19").getValue(),
+        class: sheet.getRange("J19").getValue() < 60 ? "green" : "red",
+      },
+      goal2: {
+        value: sheet.getRange("J20").getValue(),
+        class: sheet.getRange("J20").getValue() > 3.0 ? "green" : "red",
+      },
+      goal3: {
+        value: sheet.getRange("J21").getValue(),
+      },
+      goal4: {
+        value: sheet.getRange("J22").getValue(),
+      },
+      goal5: {
+        value: sheet.getRange("J23").getValue(),
+        class: sheet.getRange("J23").getValue() < 86 ? "green" : "red",
+      },
+      goal6: {
+        value: sheet.getRange("J24").getValue(),
+        class: sheet.getRange("J24").getValue() < 86 ? "green" : "red",
+      },
+    },
+  };
+
+  if (charts.length === 0) {
+    Browser.msgBox("No charts found on the specified sheet.");
+    return;
+  }
+
+  const chart1 = charts[0];
+  const chartBlob1 = chart1.getAs("image/png");
+
+  const chart2 = charts[1];
+  const chartBlob2 = chart2.getAs("image/png");
+
+  const chart3 = charts[2];
+  const chartBlob3 = chart3.getAs("image/png");
+
+  const inlineImages = {
+    chartImage1: chartBlob1,
+    chartImage2: chartBlob2,
+    chartImage3: chartBlob3,
+  };
+
+  //const recipient = "ed.edens12@gmail.com";
+  const recipient = "beatt1ks@gmail.com";
+  const subject = "Test Report";
+  const today = new Date();
+  const shortFormat = today.toLocaleDateString("en-US", { dateStyle: "short" });
+
+  // Fill in values in the HTML template
+  const template = HtmlService.createTemplateFromFile("email_template");
+  template.recipient = recipient;
+  template.goals = goals;
+  template.date = shortFormat;
+  const htmlBody = template.evaluate().getContent();
+
+  GmailApp.sendEmail(recipient, subject, "", {
+    htmlBody: htmlBody,
+    inlineImages: inlineImages,
   });
 }
